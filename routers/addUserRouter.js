6376442d069334fb087e2e341adb5878746e4fb6
Joshua <joshuadejesus12345@gmail.com>
2025-07-29 06:09:40 +0800

    completed all necessary form validations (password length, confirm)

--- routers/addUserRouter.js
+++ routers/addUserRouter.js
@@ -14,14 +14,23 @@ const validateUser = [
             throw new Error('E-mail already in use.');
         }
         return true;
-        }),
+    }),
     body('username').custom(async (value) => {
         const existingUser = await db.findUserbyUsername(value);
         if (existingUser) {
             throw new Error('Username already in use.');
         }
         return true;
-    })    
+    }),
+    body('password')
+        .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long.'),
+    body('confirm-password')
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords do not match.');
+            }
+            return true;
+        })
 ]
 
 router.get('/', addUserController.render)
