3cf20994abdd53f4727fef209ef757476e319102
Joshua <joshuadejesus12345@gmail.com>
2025-08-07 00:22:37 +0800

    changed db for railway

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,15 @@
 const { Pool } = require("pg");
-module.exports = new Pool({
-  host: "localhost",
-  user: "fopuy",
-  database: "fopuy",
-  password: "Tobirama106848!",
-  port: 5432
+//module.exports = new Pool({
+//  host: "localhost",
+//  user: "fopuy",
+//  database: "fopuy",
+//  password: 
+//  port: 5432
+//});
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, // ✅ from Railway
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
 });
+
+module.exports = pool;
